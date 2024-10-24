099bb1260310631d1f7b36b8481b5cb3b2502df9
Sundeep <sundeep.kaler@gmail.com>
2024-10-24 00:32:44 -0400

    added models to models.py

--- server/models.py
+++ server/models.py
@@ -1,6 +1,92 @@
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.ext.associationproxy import association_proxy
 
-from config import db
+from config import db, bcrypt
 
 # Models go here!
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String)
+
+    playlists =  db.relationship('Playlist', back_populates='user')
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError('Password hashes may not be viewed.')
+
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8'))
+    
+class Artist(db.Model, SerializerMixin):
+    __tablename__ = 'artists'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    biography = db.Column(db.String, nullable=False)
+    image_url = db.Column(db.String)
+
+    albums = db.relationship('Album', back_populates='artist', cascade='all, delete-orphan')
+    songs = db.relationship('Song', back_populates='artist', cascade='all, delete-orphan')
+
+class Album(db.Model, SerializerMixin):
+    __tablename__ = 'albums'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    release_date = db.Column(db.String, nullable=False)
+    image_url = db.Column(db.String)
+    
+    artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'), nullable=False)
+
+    artist = db.relationship('Artist', back_populates='albums')
+    songs = db.relationship('Song', back_populates='album')
+
+class Song(db.Model, SerializerMixin):
+    __tablename__ = 'songs'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    duration = db.Column(db.String, nullable=False)
+
+    album_id = db.Column(db.Integer, db.ForeignKey('album.id'))
+    artist_id = db.Column(db.Integer, db.ForeignKey('artist.id') nullable=False)
+
+    album = db.relationship('Album', back_populates='songs')
+    artist = db.relationship('Artist', back_populates='songs')
+
+class Playlist(db.Model, SerializerMixin):
+    __tablename__ = 'playlists'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    user = db.relationship('User', back_populates='playlists', cascade='all, delete-orphan')
+
+    songs = association_proxy('playlist_songs', 'song', creator=lambda song_obj: PlaylistSong(song=song_obj))
+
+class PlaylistSong(db.Model, SerializerMixin):
+    __tablename__ = 'playlist_songs'
+
+    id = db.Column(db.Integer, primary_key=True)
+    order = db.Column(db.Integer, unique=True)
+
+    playlist_id = db.Column(db.Integer, db.ForeignKey('playlists.id'), nullable=False)
+    song_id = db.Column(db.Integer, db.ForeignKey('songs.id'), nullable=False)
+
+    playlist = db.relationship('Playlist', back_populates='playlist_songs')
+    song = db.relationship('Song', back_populates='playlist_songs')
