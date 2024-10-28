6a5b37edfc386d9f6f44d676f1a814cd7b5640c5
Sundeep <sundeep.kaler@gmail.com>
2024-10-28 08:59:45 -0400

    adding editing/patch to artist page

--- client/src/components/ArtistPage.js
+++ client/src/components/ArtistPage.js
@@ -22,32 +22,83 @@ function ArtistPage() {
                 setIsUserArtist(true)
             }
             setArtist({
-            username: data.username,
-            biography: data.biography,
-            image_url: data.image_url,
+                username: data.username,
+                biography: data.biography,
+                image_url: data.image_url,
+            })
         })
-    })
-    }, [user, params.id])
+    }, [user, params.artistId])
+
+
+    function handleEditClick() {
+        setIsEditing(!isEditing)
+    }
+
+    function handleEditSubmit(e) {
+        e.preventDefault()
+        fetch(`/artists/${params.artistId}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                username: artist.username,
+                biography: artist.biography,
+                image_url: artist.image_url
+            })
+        })
+        .then(r  => r.json())
+        .then(data => {
+            setArtist({
+                username: data.username,
+                biography: data.biography,
+                image_url: data.image_url
+            })
+            //console.log(data)
+            setIsEditing(false)
+            alert("Profile updated successfully!")
+        })
+
+    }
+
+    const editForm = (
+        <>
+            <form onSubmit={handleEditSubmit}>
+                <label>Username:</label>
+                <input type="text" value={artist.username} onChange={(e) => setArtist({ ...artist, username: e.target.value })} />
+                <br></br>
+                <label>Biography:</label>
+                <textarea value={artist.biography} onChange={(e) => setArtist({ ...artist, biography: e.target.value })}></textarea>
+                <br></br>
+                <label>Image URL:</label>
+                <input type="text" value={artist.image_url} onChange={(e) => setArtist({ ...artist, image_url: e.target.value })} />
+                <br></br>
+                <button type="submit">Update Profile</button>
+            </form>
+            <br></br>
+            <button type="button" onClick={handleEditClick}>Cancel</button>
+        </>     
+    )
+
+        
 
     if (isUserArtist) {
-        if (isEditing) {
-            // render edit form
-            // when form is submitted, update artist data
-        }
-        else {
-            return (
-                <div>
-                    <h1>{artist.username}'s Page (You)</h1>
-                    <p>{artist.biography}</p>
-                    {isEditing ? <button onClick={setIsEditing(true)}>Edit Profile</button> : <></>}
-                </div>
-            )
-        }
-        
+        //console.log(isEditing)
+        return (
+            <>  
+                {isEditing ? editForm :
+                    <div className="ArtistDiv">
+                        <h1>{artist.username}'s Page (You)</h1>
+                        <p>{artist.biography}</p>
+                        <button name="edit-button" onClick={handleEditClick}>Edit Profile</button>
+                    </div>
+                }
+            </>
+        )
     }
     else {
         return (
-            <div>
+            <div className="ArtistDiv">
                 <h1>{artist.username}'s Page</h1>
                 <p>{artist.biography}</p>
             </div>
